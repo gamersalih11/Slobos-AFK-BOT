52f0e6d1cf93dae9cee313f8bf3a808663a8998c
gamersalih11 <gamersalih11@gmail.com>
2026-04-21 18:39:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bot 7/24 Aktif Tutuluyor!');
+});
+
+app.listen(port, () => {
+  console.log(`Bot sunucusu http://localhost:${port} adresinde dinleniyor`);
+});
+
 const mineflayer = require('mineflayer');
 const { Movements, pathfinder, goals } = require('mineflayer-pathfinder');
 const { GoalBlock } = goals;
